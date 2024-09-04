50f558b973e3d39a4190a5d1924fdd4fd99ac505
CamilaGL18 <cl2317916@gmail.com>
2024-09-04 15:46:46 -0300

    temperatura

---
+++ atv02.py
@@ -0,0 +1,9 @@
+#Crie um programa que converta uma temperatura dada em Celsius para Fahrenheit usando a fórmula: F = C * 9/5 + 32.
+celcios = int(input("temperatura"))
+
+F = celcios*9/5+32.
+
+print (F)
+
+              
+               
